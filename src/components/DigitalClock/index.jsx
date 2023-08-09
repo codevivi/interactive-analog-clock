6e6a9beb3b3dc576ab081f6ab264ba1a469b27d8
Vilma <vilmail@protonmail.com>
2023-08-09 21:16:19 +0300

    digital clock minor rename, and move func out

--- src/components/DigitalClock/index.jsx
+++ src/components/DigitalClock/index.jsx
@@ -3,29 +3,31 @@ import "./style.css";
 import { SettingsCtx } from "../../context/SettingsCtx";
 import { TimeCtx } from "../../context/TimeCtx";
 
+function formatTimeToString(n) {
+  let str = "" + n;
+  if (str.length < 2) {
+    str = "0" + str;
+  }
+  return str;
+}
+function formatDigitalHour(num, isHourFormat12) {
+  if (isHourFormat12) {
+    if (num > 12) {
+      num = num - 12;
+    }
+  }
+  return num;
+}
+
 const DigitalClock = () => {
   const { isInteractive, isHourFormat12 } = useContext(SettingsCtx);
   const { seconds, minutes, hours } = useContext(TimeCtx);
-  function pad2(n) {
-    let str = "" + n;
-    if (str.length < 2) {
-      str = "0" + str;
-    }
-    return str;
-  }
-  function formatDigitalHour(num) {
-    if (isHourFormat12) {
-      if (num > 12) {
-        num = num - 12;
-      }
-    }
-    return num;
-  }
+
   return (
     <p className="digital unselectable" onMouseOver={(e) => e.stopPropagation()}>
-      <small className="digital__hours-display">{pad2(formatDigitalHour(hours))}:</small>
-      <small className="digital__minutes-display">{pad2(minutes)}</small>
-      {!isInteractive && <small className="digital__seconds-display">:{pad2(seconds)}</small>}
+      <small className="digital__hours-display">{formatTimeToString(formatDigitalHour(hours, isHourFormat12))}:</small>
+      <small className="digital__minutes-display">{formatTimeToString(minutes)}</small>
+      {!isInteractive && <small className="digital__seconds-display">:{formatTimeToString(seconds)}</small>}
     </p>
   );
 };
